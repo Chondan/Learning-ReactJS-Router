7c225d8303f9d1cbc350ccc8764c2b24fe9af8f1
Chondan <chondansusuwan@outlook.com>
2020-10-10 00:51:35 +0700

    uploade build folder for publishing on github

---
+++ precache-manifest.2dfc67f77d24bda7f8c686eb3c7c9e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9547d42fe0474b8fb3dddd66be7ddb",
+    "url": "/Learning-ReactJS-Router/index.html"
+  },
+  {
+    "revision": "f97db811c6e957987916",
+    "url": "/Learning-ReactJS-Router/static/css/main.09ce0382.chunk.css"
+  },
+  {
+    "revision": "db543a9435381224421a",
+    "url": "/Learning-ReactJS-Router/static/js/2.5fbf8925.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Learning-ReactJS-Router/static/js/2.5fbf8925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97db811c6e957987916",
+    "url": "/Learning-ReactJS-Router/static/js/main.ab4b9f69.chunk.js"
+  },
+  {
+    "revision": "b0e03d15e2d01d20134f",
+    "url": "/Learning-ReactJS-Router/static/js/runtime-main.a15b3fc0.js"
+  }
+]);
